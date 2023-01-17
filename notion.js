6d417df37d6c5901dbd471472d5fc0477bba846a
trejoszx1 <trejoszx1@gmail.com>
2023-01-17 17:30:27 -0500

    the frontend was created and the script for the vote button was created

--- notion.js
+++ notion.js
@@ -1,50 +1,152 @@
-const { Client } = require('@notionhq/client');
+require("dotenv").config();
 
-const notion = new Client({ auth: process.env.NOTION_TOKEN})
+const { Client } = require("@notionhq/client");
 
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-function createSuggestion({title, stock , numbers, detalles}){
-    notion.pages.create({
-        parent:{
-            database_id: process.env.NOTION_DATABASE_ID
-        },
-        properties: {
-//            [process.env.NOTION_TITLE_ID]:{
-//                title:[{
-//                    type: 'text',
-//                    text:
-//                    {
-//                        content: title,
-//                    },
-//                },
-//                ],
-//            },
-            [process.env.NOTION_DETALLES_ID]: {
-                    rich_text: [
-                      {
-                        type: "text",
-                        text: {
-                          content: detalles,
-                        },
-                      },
-                    ],
-                },
-//           [process.env.NOTION_STOCK_ID]:{
-//                    checkbox: stock
-//                    },
-//            [process.env.NOTION_NUMBER_ID]:{
-//                number: numbers
-//                    },
-//
-        },
-    })
+async function getTags() {
+  const database = await notion.databases.retrieve({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+  //console.log(database.properties.Selecioness.select) // VER ESPECIFICO SELECT
+  //console.log("multi select",database.properties.multi.multi_select) // VER ESPECIFICO SELECT
 
+  //console.log(notionPropertiesById(database.properties))
+
+  return notionPropertiesById(database.properties)[
+    process.env.NOTION_TAGS_ID
+  ].multi_select.options.map((option) => {
+    return { id: option.id, name: option.name };
+  });
 }
 
-createSuggestion({
-    title:'Vanessa Heranadez',
-    numbers:100123123123,
-    stock: true,
-    detalles:'es un nina linda',
-})
+function notionPropertiesById(properties) {
+  return Object.values(properties).reduce((obj, property) => {
+    const { id, ...rest } = property;
+    return { ...obj, [id]: rest };
+  }, {});
+}
+
+function createSuggestion({ title, isProject, number, description, tags }) {
+  notion.pages.create({
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ID,
+    },
+    properties: {
+      ///---Name---///
+      [process.env.NOTION_TITLE_ID]: {
+        title: [
+          {
+            type: "text",
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      //            "Checkbox":{   // tiambien se puede usar con el nombre de la columna
+      //              "rich_text": [{
+      //                  type: 'text',
+      //                  text:{
+      //                  content: 'a'
+      //                  }
+      //                }
+      //
+      //            ],},
+      [process.env.NOTION_DETALLES]: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: description,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_STOCK_ID]: {
+        checkbox: isProject,
+      },
+      [process.env.NOTION_NUMBER_ID]: {
+        number: 0,
+      },
+
+      ///--Selection---///
+      [process.env.NOTION_TAGS_ID]: {
+        multi_select: tags.map((tag) => {
+          return { id: tag.id };
+        }),
+      },
+    },
+  });
+}
+
+//getTags().then(res => console.log(res))
+
+async function getSuggestions() {
+  const notionPages = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    sorts: [
+      {
+        property: process.env.NOTION_NUMBER_ID,
+        direction: "descending",
+      },
+    ],
+  });
+
+  return notionPages.results.map(fromNotionObject);
+}
+
+function fromNotionObject(notionPages) {
+  const propertiesById = notionPropertiesById(notionPages.properties);
+  return {
+    id: notionPages.id,
+    title: propertiesById[process.env.NOTION_TITLE_ID].title[0].plain_text,
+    number: propertiesById[process.env.NOTION_NUMBER_ID].number,
+    tags: propertiesById[process.env.NOTION_TAGS_ID].multi_select.map(
+      (option) => {
+        return { id: option.id, name: option.name };
+      }
+    ),
+    isProject: propertiesById[process.env.NOTION_STOCK_ID].checkbox,
+    description:
+      propertiesById[process.env.NOTION_DETALLES].rich_text[0].text.content,
+  };
+}
+async function upVoteSuggestion(pageId){
+  const suggestion = await getSuggestion(pageId)
+  const votes = suggestion.votes + 1
+  await notion.pages.update({
+    page_id: pageId,
+    properties:{
+      [process.env.NOTION_NUMBER_ID]: { number: votes }
+    }
+  })
+  return votes
+};
+
+async function getSuggestion(pageId){
+  return fromNotionObject(await notion.pages.retrieve({ page_id: pageId }))
+}
+
+module.exports = {
+  createSuggestion,
+  getTags,
+  getSuggestions,
+  upVoteSuggestion,
+};
+
+////------Send data--------////
+//
+//getTags().then(tags => {
+// createSuggestion({
+//    title:'david ',
+//    numbers:100123123123,
+//    stock: true,
+//    detalles:'es un nina linda',
+//    tags: tags,
+//})()
+//
+//})
+//
+
 //https://www.youtube.com/watch?v=zVfVLBjQuSA
--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 require('dotenv').config();
 const express = require("express");
-const notion = require("./notion")
+const { getTags, createSuggestion, getSuggestions, upVoteSuggestion} = require("./notion")
+
+
 
 const app = express();
+app.set('views','./views')
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
-app.get('/',(req,res) =>{
-    res.send("hi")
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+
+let tags = []
+
+getTags().then(data => {
+    tags = data
+} )
+
+setInterval(async()=>{
+    tags = await getTags()
+}, 1000 * 60 * 60)
+
+
+
+app.get('/',async (req,res) =>{
+    const suggestions = await getSuggestions()
+    res.render('index', {tags, suggestions})
+})
+app.post('/create-user', async (req, res) =>{
+    const{ title,  description, isProject, tagIds /*number*/ = [] } = req.body
+    
+
+    await createSuggestion({
+        title, 
+        isProject: isProject != null, 
+        //numbers: number, 
+        description, 
+        tags: (Array.isArray(tagIds) ? tagIds: [tagIds]).map(tagId => {
+            return { id: tagId}
+        })
+    })
+    res.redirect("/")
+
+})
+app.post('/up-vote-suggestion', async (req, res) => {
+    const votes = await upVoteSuggestion(req.body.suggestionId)
+    res.json({ votes })
 })
 
 app.listen(process.env.PORT)
